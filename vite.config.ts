0b286652ba5d0765a23e3200922f71286cf920c3
jfilho <jorgejuniords@gmail.com>
2026-09-19 01:40:04 -0300

    Novas alterações para o vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite"; // <-- importe o plugin
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { nitro } from "nitro/vite"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
-    tailwindcss(), // <-- adicionado primeiro
+    tailwindcss(),
     tanstackStart({
       server: { entry: "src/server" },
     }),
-    nitro({
-        preset: "vercel", // Essencial para o deploy na Vercel
-      }),
     react(),
+    nitro({
+      preset: "vercel",
+      output: {
+        dir: ".vercel/output",
+        serverDir: ".vercel/output/functions/__server.func",
+        publicDir: ".vercel/output/static",
+      },
+    }),
   ],
   resolve: {
     alias: {
