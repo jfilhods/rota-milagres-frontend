7123c106ec0c99e21b4b6b4d6692550c00159ccc
jfilho <jorgejuniords@gmail.com>
2026-09-19 01:02:45 -0300

    Retirada do render do vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite"; // 1. Importe o nitro
+import tailwindcss from "@tailwindcss/vite"; // <-- importe o plugin
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -11,15 +10,11 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss(), // <-- adicionado primeiro
     tanstackStart({
       server: { entry: "src/server" },
     }),
     react(),
-    // 2. Adicione o plugin nitro com o renderer desabilitado
-    nitro({
-      renderer: false, // Impede o Nitro de usar o index.html da raiz
-    }),
   ],
   resolve: {
     alias: {
